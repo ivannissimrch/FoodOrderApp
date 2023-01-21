4abcf4058e05788eb596f3ce6fdd0cfeed6b1377
Ivan <ivannissimrch@gmail.com>
2023-01-20 21:18:57 -0500

    before add form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Fragment } from "react";
 import Header from "./components/Layout/Header";
+import AvaliableMeals from "./components/Meals/AvaliableMeals";
+import MealsSummary from "./components/Meals/MealsSummary";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <MealsSummary />
+        <AvaliableMeals />
+      </main>
     </Fragment>
   );
 }
